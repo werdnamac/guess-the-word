3dd7fc3f63bbc8d05315529db7b8cdfc210c48a9
andy <werdnamac@gmail.com>
2022-05-09 18:55:55 -0400

    Added an array for the guessed letters and a way to check which guessed letters have already been added and to add those that haven't already been added to the array.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 let word = "magnolia";
+const guessedLettersArray = [];
 
 const addCircles = function (word) {
     const circles = []
@@ -23,9 +24,45 @@ addCircles(word);
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
     
-    const input = inputLetter.value;
-    console.log(input);
+    const guess = inputLetter.value;
+    //console.log(input);
+    message.innerText = "";
+    const goodGuess = validate(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    }
     inputLetter.value = "";
 
 });
+
+const validate = function (input) {
+    const acceptedLetter = /[a-zA-z]/;
+    if (input.length === 0) {
+        message.innerText = "You didn't enter anything. Please enter a letter."
+    } else if (input.length > 1) {
+        message.innerText = "Please enter only one letter."
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "That's not a letter! Please enter a letter."
+    } else {
+        return input;
+        
+    }
+
+};
+
+const makeGuess = function(guess) {
+    let allCapsGuess = guess.toUpperCase();
+    
+    //they used console.log(guessedLettersArray); here
+    if (guessedLettersArray.indexOf(allCapsGuess) !== -1) {
+        message.innerText = "You already guessed that letter. Please try again."
+    } else {
+        guessedLettersArray.push(allCapsGuess);
+        console.log(guessedLettersArray);
+    }
+    
+};
+
+
 
